4884482a0839f277fd7254920ccc1f829ba95608
evan <xujianfei163.com>
2018-01-11 14:12:06 +0800

    update asset update task.

--- cmdb/tasks.py
+++ cmdb/tasks.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import, unicode_literals
+from datetime import datetime
 
 from celery import shared_task
 import pepper
 
 from DEVOPS.settings import SALT_API
-from cmdb.models import Asset
+from cmdb.models import Asset, IpAddress
 
 
 @shared_task
@@ -12,28 +13,80 @@ def add(x, y):
     return x + y
 
 
+def _update_asset(obj, v):
+    """
+    Update asset if asset exists
+    :param obj: Asset object
+    :param v: single grains return value
+    :return:
+    """
+    host_type = 0 if v['virtual'] == 'physical' else 1
+    obj.hostname = v['host']
+    obj.asset_type = host_type
+    obj.vendor = v['manufacturer']
+    obj.sn = v['serialnumber']
+    obj.cpu_model = v['cpu_model']
+    obj.cpu_cores = v['num_cpus']
+    obj.memory = v['mem_total']
+    obj.disk_total = '100G'
+    obj.disk_info = '/'
+    obj.kernel = v['kernel']
+    obj.kernel_version = v['kernelrelease']
+    obj.os = v['os']
+    obj.os_version = v['osrelease']
+    obj.os_arch = v['osarch']
+
+    obj.ip.clear()
+    for ip in v['ipv4'].remove('127.0.0.1'):
+        obj.ip.add(IpAddress.objects.get(ip_address=ip))
+
+    obj.update_date = datetime.now()
+
+    obj.save()
+
+
+def _create_asset(v):
+    """
+    Create asset if not exists.
+    :param v: single grains return value
+    :return:
+    """
+    host_type = 0 if v['virtual'] == 'physical' else 1
+    asset_obj = Asset(hostname=v['host'], asset_type=host_type, vendor=v['manufacturer'],
+                      sn=v['serialnumber'], cpu_model=v['cpu_model'], cpu_cores=v['num_cpus'],
+                      memory=v['mem_total'], disk_total='100G', disk_info='/', kernel=v['kernel'],
+                      kernel_version=v['kernelrelease'], os=v['os'], os_version=v['osrelease'],
+                      os_arch=v['osarch'], comment='created from salt.')
+
+    for ip in v['ipv4'].remove('127.0.0.1'):
+        asset_obj.ip.add(IpAddress.objects.get(ip_address=ip))
+
+    asset_obj.save()
+
+
 @shared_task
 def update_asset_info(asset=None):
+    """
+    :param asset: should be list type
+    :return: None
+    """
     salt_api = pepper.Pepper(SALT_API['url'], ignore_ssl_errors=True)
     salt_api.login(SALT_API['username'], SALT_API['password'], eauth='pam')
 
     if asset:
-        pass
-        ret = None
+        ret = salt_api.local(asset, 'grains.items', expr_form='list')['return'][0]
     else:
         ret = salt_api.local('*', 'grains.items')['return'][0]
-        print(ret)
 
     for k, v in ret.items():
-        host_type = 0 if v['virtual'] == 'physical' else 1
-        ip = None
-        Asset.objects.create(hostname=v['host'], asset_type=host_type, vendor=v['manufacturer'],
-                             sn=v['serialnumber'], cpu_model=v['cpu_model'], cpu_cores=v['num_cpus'],
-                             memory=v['mem_total'], disk_total='100G', disk_info='/', kernel=v['kernel'],
-                             kernel_version=v['kernelrelease'], os=v['os'], os_version=v['osrelease'],
-                             os_arch=v['osarch'], comment='created from salt.')
-
-    # TODO：更新ip地址，处理资产更新和单资产更新
+        if Asset.objects.filter(sn=v['serialnumber']).exists():
+            obj = Asset.objects.get(sn=v['serialnumber'])
+            _update_asset(obj, v)
+        elif Asset.objects.filter(ip=IpAddress.objects.get(ip_address=k)).exists():
+            obj = Asset.objects.get(ip=IpAddress.objects.get(ip_address=k))
+            _update_asset(obj, v)
+        else:
+            _create_asset(v)
 
 
 if __name__ == '__main__':
